6be8e0f283cc011f2e931201df7df957f2353e02
Takuto Odagawa <xtctl.1234@gmail.com>
2019-04-21 11:12:02 +0000

    Add UserMailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
 class User < ApplicationRecord
   
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email # defined in private
+  before_create :create_activation_digest
   
-  before_save { email.downcase! } # self.email = email.downcase
+  # before_save { email.downcase! } # self.email = email.downcase
   
   validates :name, presence: true, length: { maximum: 50 }
   
@@ -43,5 +45,17 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+    # メールアドレスをすべて小文字にする
+    def downcase_email
+      email.downcase! # self.email = email.downcase
+    end
+
+    # 有効化トークンとダイジェストを作成および代入する
+    def create_activation_digest
+      self.activation_token  = User.new_token
+      self.activation_digest = User.digest(activation_token)
+    end
 
 end
